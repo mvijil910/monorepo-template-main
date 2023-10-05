0a3e10f32423693f73fd315db9808ba62c068658
Marcela Vijil <marcelavijil@Marcelas-MacBook-Pro.local>
2023-10-05 14:07:12 +0200

    adding golden master text file & updated test class

--- Assignment3/part1/test_gilded_rose.py
+++ Assignment3/part1/test_gilded_rose.py
@@ -10,6 +10,29 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals("fixme", items[0].name)
+    
+    def test_conjured_item(self):
+        # Arrange
+        items = [Item("Conjured", 5, 42)]
+        gr = GildedRose(items)
+
+        original_sell_in = gr.get_item_sell_in("Conjured")
+        original_quality = gr.get_item_quality("Conjured")
+
+        # Act
+        gr.update_quality()
+
+        # Assert
+        new_sell_in = gr.get_item_sell_in("Conjured")
+        new_quality = gr.get_item_quality("Conjured")
+
+        assert new_sell_in < original_sell_in
+        assert new_sell_in == original_sell_in - 1
+
+        assert new_quality > -1
+        assert new_quality <= 50
+        assert new_quality < original_quality
+        assert new_quality == original_quality - 2
 
 
 if __name__ == '__main__':
