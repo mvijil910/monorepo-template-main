635186d743d4261755293d92b10bf09d9114da67
Marcela Vijil <marcelavijil@Marcelas-MacBook-Pro.local>
2023-10-05 20:22:06 +0200

    updating tests and gilded rose

--- Assignment3/part1/gilded_rose.py
+++ Assignment3/part1/gilded_rose.py
@@ -17,33 +17,67 @@ class GildedRose(object):
     def __init__(self, items: list[Item]):
         # DO NOT CHANGE THIS ATTRIBUTE!!!
         self.items = items
-
+    
     def update_quality(self):
-        for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+        
+        def update_qual_conjured(qual):
+            if qual < 50: 
+                if qual > 0: 
+                    if qual > 2: 
+                        qual = qual - 2
+                    else: 
+                        qual = qual -1
+                    return qual
+                else: 
+                    return 0
+                
+            return 50
+        
+        def update_qual_agedBrie(qual): 
+            if qual < 50:
+                qual = qual + 1
+            return qual 
+        
+        def update_qual_bkstg(sellIn, qual): 
+            if qual < 50:
+                if sellIn < 11:
+                    if sellIn < 0: 
+                        qual = 0
+                    if sellIn < 6:
+                        qual += 3
+                    else: 
+                        qual += 2
+                    return qual
+            else: 
+                qual = 50
+            return qual 
+        
+        def update_qual_sulfuras(qual):
+            qual = 80
+            return qual
+        
+        for item in self.items: 
+            item.sell_in = item.sell_in - 1
+            if item.name == "Aged Brie": 
+                item.quality = update_qual_agedBrie(item.quality)
+            elif item.name == "Conjured": 
+                item.quality = update_qual_conjured(item.quality)
+            elif item.name == "Sulfuras, Hand of Ragnaros": 
+                item.quality = update_qual_sulfuras(item.quality) 
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert": 
+                item.quality = update_qual_bkstg(item.sell_in, item.quality)    
+            else: 
+                if item.quality > 0 and item.quality < 50: 
+                    item.quality = item.quality - 1
+                
+                              
+    
+    def get_item_sell_in(self, itemName): 
+        for item in self.items: 
+            if itemName == item.name: 
+                return item.sell_in
+    
+    def get_item_quality(self, itemName): 
+        for item in self.items: 
+            if itemName == item.name:
+                return item.quality
--- Assignment3/part1/test_gilded_rose.py
+++ Assignment3/part1/test_gilded_rose.py
@@ -9,7 +9,7 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals("foo", items[0].name)
     
     def test_conjured_item(self):
         # Arrange
@@ -34,6 +34,48 @@ class GildedRoseTest(unittest.TestCase):
         assert new_quality < original_quality
         assert new_quality == original_quality - 2
 
+    
+    def test_sulfuras_item(self):
+        # Arrange
+        items = [Item("Sulfuras, Hand of Ragnaros", -1, 49)]
+        gr = GildedRose(items)
+
+        original_sell_in = gr.get_item_sell_in("Sulfuras, Hand of Ragnaros")
+        original_quality = gr.get_item_quality("Sulfuras, Hand of Ragnaros")
+
+        # Act
+        gr.update_quality()
+
+        # Assert
+        new_sell_in = gr.get_item_sell_in("Sulfuras, Hand of Ragnaros")
+        new_quality = gr.get_item_quality("Sulfuras, Hand of Ragnaros")
+
+        assert new_sell_in < original_sell_in
+
+        assert new_quality == 80
+
+    def test_bkstg_item(self):
+        # Arrange
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 5, 46)]
+        gr = GildedRose(items)
+
+        original_sell_in = gr.get_item_sell_in("Backstage passes to a TAFKAL80ETC concert")
+        original_quality = gr.get_item_quality("Backstage passes to a TAFKAL80ETC concert")
+
+        # Act
+        gr.update_quality()
+
+        # Assert
+        new_sell_in = gr.get_item_sell_in("Backstage passes to a TAFKAL80ETC concert")
+        new_quality = gr.get_item_quality("Backstage passes to a TAFKAL80ETC concert")
+
+        assert new_sell_in < original_sell_in
+        assert new_sell_in == original_sell_in - 1
+
+        assert new_quality > -1
+        assert new_quality <= 50
+        assert new_quality > original_quality
+        assert new_quality == original_quality + 3
 
 if __name__ == '__main__':
     unittest.main()
